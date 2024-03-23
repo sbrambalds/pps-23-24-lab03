4b97898ec0c842d8f9f1973d388382bcf5dec304
Riccardo Rambaldi <riccardo.rambaldi4@studio.unibo.it>
2024-03-23 15:55:47 +0100

    Add return type to TestAll's methods

--- src/test/scala/lab3/TestAll.scala
+++ src/test/scala/lab3/TestAll.scala
@@ -14,63 +14,63 @@ class SequenceTest:
 
   val l: Sequence[Int] = Cons(10, Cons(20, Cons(30, Nil())))
 
-  @Test def testSum() =
+  @Test def testSum(): Unit =
     assertEquals(0, Nil().sum)
     assertEquals(60, l.sum)
 
-  @Test def testMap() =
+  @Test def testMap(): Unit =
     assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), l.map(_ + 1))
     assertEquals(Cons("10", Cons("20", Cons("30", Nil()))), l.map(_ + ""))
 
-  @Test def testFilter() =
+  @Test def testFilter(): Unit =
     assertEquals(Cons(20, Cons(30, Nil())), l.filter(_ >= 20))
     assertEquals(Cons(10, Cons(30, Nil())), l.filter(_ != 20))
   
-  @Test def testTake() =
+  @Test def testTake(): Unit =
     assertEquals(Cons(10, Cons(20, Nil())), l.take(2))
     assertEquals(Cons(10, Cons(20, Cons(30, Nil()))), l.take(3))
     assertEquals(Nil(), l.take(0))
     assertEquals(Nil(), Nil().take(2))
   
-  @Test def testZip() = 
+  @Test def testZip(): Unit = 
     val l2: Sequence[String] = Cons("10", Cons("20", Cons("30", Nil())))
     assertEquals(Cons((10, "10"), Cons((20, "20"), Cons((30, "30"), Nil()))), l.zip(l2))
     assertEquals(Nil(), l.zip(Nil()))
     assertEquals(Nil(), Nil().zip(l2))
     assertEquals(Nil(), Nil().zip(Nil()))
 
-  @Test def testConcat() =
+  @Test def testConcat(): Unit =
     val l2: Sequence[Int] = Cons(40, Cons(50, Nil()))
     assertEquals(Cons(10, Cons(20, Cons(30, Cons(40, Cons(50, Nil()))))), l.concat(l2))
     assertEquals(Cons(40, Cons(50, Nil())), Nil().concat(l2))
     
-  @Test def testFlatMap() =
+  @Test def testFlatMap(): Unit =
     assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), l.flatMap(v => Cons(v + 1, Nil())))
     assertEquals(Nil(), Nil().flatMap(v => Cons(v, Nil())))
 
     //My Tests:
-  @Test def testMin() =
+  @Test def testMin(): Unit =
     assertEquals(Optional.Just(10), l.min)
     assertEquals(Optional.Just(1), Cons(1, Nil()).min)
     assertEquals(Optional.Empty(), Nil().min)
 
-  @Test def testCourses() =
+  @Test def testCourses(): Unit =
     val withTeacher: Sequence[Person] = Cons(Person.Teacher("marco", "pcd"), Cons(Person.Teacher("franco", "pps"), Cons(Person.Student("dario", 2000), Nil())))
     val withoutTeacher: Sequence[Person] = Cons(Person.Student("marco", 2001), Cons(Person.Student("dario", 2000), Nil()))
     assertEquals(Cons("pcd", Cons("pps", Nil())), withTeacher.courses)
     assertEquals(Nil(), withoutTeacher.courses)
 
-  @Test def testCourses2() =
+  @Test def testCourses2(): Unit =
     val persons: Sequence[Person] = Cons(Person.Teacher("marco", "pcd"), Cons(Person.Teacher("franco", "pps"), Cons(Person.Student("dario", 2000), Nil())))
     assertEquals(Cons("pcd", Cons("pps", Nil())), persons.courses2)
 
-  @Test def testFoldLeftWithSameType() = 
+  @Test def testFoldLeftWithSameType(): Unit = 
     val res = -60
     val l2: Sequence[Int] = Nil()
     assertEquals(res, l.foldLeft(0)(_ - _))
     assertEquals(10, l2.foldLeft(10)(_ - _))
   
-  @Test def testFoldLeftWithDifferentTypes() =
+  @Test def testFoldLeftWithDifferentTypes(): Unit =
     val res = 60.5
     assertEquals(res, l.foldLeft(0.5)(_ + _), 0)
 
